b17b41aa57753eebdad6a71b881273857adbeea7
rayhanmahmud98 <rayhaniiuc48@gmail.com>
2025-08-03 22:12:55 +0600

    Updated component structure

--- Django/core/home/views.py
+++ Django/core/home/views.py
@@ -5,7 +5,27 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,"index.html")
+    context = {'page':'home'}
+    
+    peoples = [
+    {'name': 'rayhan', 'age': 26},
+    {'name': 'sarah', 'age': 14},
+    {'name': 'alex', 'age': 30},
+    {'name': 'gg','age': 15},
+    {'name': 'michael', 'age': 28}
+]
+    
+
+    
+    return render(request,"index.html", context = {'peoples' : peoples})
 
 def success_page(request):
     return HttpResponse("Congratulation for your success!!!!!!!!!!")
+
+def about(request):
+    context = {'page':'About'}
+    return render(request,"about.html",context)
+
+def contact(request):
+    context = {'page':'Contact'}
+    return render(request,"contact.html",context)
