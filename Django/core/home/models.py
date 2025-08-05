c9b2226d6d845286a58b73c532b9d42a27b6c262
rayhanmahmud98 <rayhaniiuc48@gmail.com>
2025-08-05 21:03:29 +0600

    Updated component structure

--- Django/core/home/models.py
+++ Django/core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    # image = models.ImageField()
+    file = models.FileField()
+    birth_date = models.DateField()
+    cgpa = models.DecimalField(max_digits=10,decimal_places=2)
