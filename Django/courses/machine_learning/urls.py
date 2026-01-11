d13b04ed8b5c235b34ac3d1de698d02e428f6bea
rayhanmahmud98 <rayhaniiuc48@gmail.com>
2026-01-12 03:15:47 +0600

    learned about templates

--- Django/courses/machine_learning/urls.py
+++ Django/courses/machine_learning/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('machine/', views.machine, name='machine'),
+    path('machine/', views.machine),
+    path('dt/', views.dt),
+    path('random/', views.random),
+    path('knn/', views.knn),
 ]
--- Django/courses/machine_learning/views.py
+++ Django/courses/machine_learning/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def machine(request):
-    return render(request, 'machine_learning.html')
+    return render(request, 'machine_learning/machine_learning.html')
+
+def random(request):
+    return render(request, 'machine_learning/random_forest.html')
+
+def knn(request):
+    return render(request, 'machine_learning/knn.html')
+
+def dt(request):
+    return render(request, 'machine_learning/dt.html')
